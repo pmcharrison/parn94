e20995613e12ea7f6c1c86f1de86440dcc0d59cd
Peter M C Harrison <pmc.harrison@gmail.com>
2018-12-17 14:47:33 +0000

    Documenting params

--- R/params.R
+++ R/params.R
@@ -1,13 +1,27 @@
-#' Get parameters for Parncutt's model
+#' Model parameters
 #'
-#' Gets parameters for Parncutt's psychoacoustic model.
-#' @param k_t Numeric scalar; parameter from \insertCite{Parncutt1994;textual}{parn94}.
-#' @param k_p Numeric scalar; parameter from \insertCite{Parncutt1994;textual}{parn94}.
-#' @param k_c Numeric scalar; parameter from \insertCite{Parncutt1994;textual}{parn94}.
-#' @param k_s Numeric scalar; parameter from \insertCite{Parncutt1994;textual}{parn94}.
-#' @param al_0 Numeric scalar; parameter from \insertCite{Parncutt1994;textual}{parn94}.
-#' @param min_midi Numeric scalar; the lowest MIDI pitch considered in the psychoacoustic model.
-#' @param max_midi Numeric scalar; the highest MIDI pitch considered in the psychoacoustic model.
+#' This function compiles parameters for Parncutt's psychoacoustic model.
+#' The parameters are defined with reference to
+#' \insertCite{Parncutt1994;textual}{parn94}.
+#' @param unit_amplitude_in_dB (Numeric scalar) Describes the number of decibels
+#' to assign to a spectral component of amplitude 1.
+#' By default, amplitude 1 corresponds to the amplitude of each chord pitch's
+#' fundamental frequency (though this can be changed by expressing input chords
+#' as pitch-class sparse spectra, see \code{\link[hrep]{pi_sparse_spectrum}()}).
+#' @param template_num_harmonics (Integerish scalar) Number of harmonics to
+#' include in the spectral template, including the fundamental frequency.
+#' @param template_roll_off (Numeric scalar) Roll-off rate for the
+#' harmonic template. This parameter is passed to
+#' \code{\link[hrep]{pi_sparse_spectrum}()}.
+#' @param k_t (Numeric scalar) Relative perceptual weighting of complex versus pure tones.
+#' @param k_p (Numeric scalar) Scaling coefficient for pure sonorousness.
+#' @param k_c (Numeric scalar) Scaling coefficient for complex sonorousness.
+#' @param k_s (Numeric scalar) Scaling coefficient for multiplicity.
+#' @param al_0 (Numeric scalar) Audibility saturation rate.
+#' @param min_midi (Numeric scalar) Lowest MIDI pitch considered by the model.
+#' @param max_midi (Numeric scalar) Highest MIDI pitch considered by the model.
+#' @return A list of parameters which can be passed to analysis functions
+#' such as \code{\link{parn94}}.
 #' @references
 #' \insertAllCited{}
 #' @export
@@ -15,7 +29,6 @@ parn94_params <- function(
   unit_amplitude_in_dB = 60,
   template_num_harmonics = 11,
   template_roll_off = 1,
-  template_stretched_octave = FALSE,
   k_t = 3,
   k_p = 0.5,
   k_c = 0.2,
@@ -23,9 +36,7 @@ parn94_params <- function(
   k_m = 12,
   al_0 = 15,
   min_midi = 0,
-  max_midi = 120,
-  auditory_model = TRUE,
-  peak_method = "peak"
+  max_midi = 120
 ) {
   as.list(environment())
 }
